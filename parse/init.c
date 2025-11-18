dcdfc571e072ba0791197139e6dbf85d0e1eca42
Elisa Fittante <efittant@c3r7p6.42vienna.com>
2025-11-18 17:19:39 +0100

    init texture structs protected

--- parse/init.c
+++ parse/init.c
@@ -21,31 +21,28 @@ void	init(t_mlx *data, int nb_lines)
 	//textures 
 	data->input->n_texture = malloc(sizeof(t_texture));
 	if (!data->input->n_texture)
-		return;	//malloc error
+		return (parse_error(data, ERR_ALLOC, 1));
 	memset(data->input->n_texture, 0, sizeof(t_texture));
 	data->input->n_texture->file_name = NULL;
 
 	data->input->s_texture = malloc(sizeof(t_texture));
 	if (!data->input->s_texture)
-		return;	//malloc error
+		return (parse_error(data, ERR_ALLOC, 1));
 	memset(data->input->s_texture, 0, sizeof(t_texture));
 	data->input->s_texture->file_name = NULL;
 
 	data->input->w_texture = malloc(sizeof(t_texture));
 	if (!data->input->w_texture)
-		return;	//malloc error
+		return (parse_error(data, ERR_ALLOC, 1));
 	memset(data->input->w_texture, 0, sizeof(t_texture));
 	data->input->w_texture->file_name = NULL;
 
 	data->input->e_texture = malloc(sizeof(t_texture));
 	if (!data->input->e_texture)
-		return;	//malloc error
+		return (parse_error(data, ERR_ALLOC, 1));
 	memset(data->input->e_texture, 0, sizeof(t_texture));
 	data->input->e_texture->file_name = NULL;
 
-	/* data->input->s_texture->file_name = NULL;
-	data->input->e_texture->file_name = NULL;
-	data->input->w_texture->file_name = NULL; */
 	data->mlx = NULL;
 	data->window = NULL;
 	data->input->line_length = 0;
